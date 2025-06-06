dbf78aff2498c9a1b9982f4d3e8d64d14440e460
Aditya Chavan <chavanaditya9325@gmail.com>
2025-06-06 21:20:45 +0530

    Implement like functionality for videos, comments, and tweets in like.controller.js

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,92 @@
+import {Like} from '../models/like.model.js'
+import {ApiError} from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+    const {videoId} = req.params
+    //TODO: toggle like on video
+
+    if(!videoId){
+        throw new ApiError(400, "Video ID is required")
+    }
+
+    const existingLike = await Like.findOne({video: videoId, likedBy: req.user?._id})
+
+    if(existingLike){
+        await Like.deleteOne({video : videoId, likedBy: req.user?._id})
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, {}, "Like remove successfully")
+        )
+    }else{
+        const newLike = await Like.create({
+            video: videoId,
+            likedBy: req.user?._id
+        })
+
+        if(!newLike){
+            throw new ApiError(500, "failed to like the video")
+        }
+        return res
+        .status(201)
+        .json(
+            new ApiResponse(201, newLike, "Video liked successfully")
+        )
+    }
+})
+    
+    
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params
+    //TODO: toggle like on comment
+
+      if (!commentId){
+        throw new ApiError(400, "Comment ID is not found")
+    }
+
+    const existingLikedComment = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    if(existingLikedComment){
+        await Like.findByIdAndDelete(existingLikedComment._id)
+        return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Comment Like is removed successfully "))
+    }
+
+    const newLike = await Like.create({
+        comment : commentId,
+        likedBy : req.user?._id
+    })
+
+    if(!newLike){
+        throw new ApiError(500, "Failed to like the comment")
+    }
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, newLike, "comment Liked successfully"))
+
+})
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params
+    //TODO: toggle like on tweet
+}
+)
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+    //TODO: get all liked videos
+})
+
+export {
+    toggleCommentLike,
+    toggleTweetLike,
+    toggleVideoLike,
+    getLikedVideos
+}
