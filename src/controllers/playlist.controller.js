0c649247cbc08584be3bece66744d8cac79904be
Aditya Chavan <chavanaditya9325@gmail.com>
2025-06-13 20:40:12 +0530

    Fix import statement and implement user playlist fetching functionality

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
+import {Playlist} from "../models/playlist.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-import {Playlist} from "../models/playlist.model.js"
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
@@ -36,6 +35,31 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+    if(!userId){
+        throw new ApiError(400, "userId is required")
+    }
+
+    const playlists = await Playlist.find({owner: userId}).
+    populate("videos", "title thumbnailUrl")
+    .populate("owner", "name profilePicture")
+
+    // This is how response will look like when we use populate
+    // "owner"{
+    //     "name": "Aditya",
+    //     "profilePicture": "https://example.com/profile.jpg"
+    // }
+
+    if(!playlists || playlists.length === 0){
+        throw new ApiError(404, "No playlists found for this user")
+    }
+
+    return res 
+    .status(200)
+    .json(
+        new ApiResponse(200, playlists, "User playlists fetched successfully")
+    )
+
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
